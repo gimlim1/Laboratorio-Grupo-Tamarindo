934dc23781ab2e394d4776cfe8023bad2f1b924b
gimlim 1 <ricardojosepineda@hotmail.com>
2015-11-07 14:57:45 -0600

    carpeta

---
+++ OperacionesCalculadora/OperacionesCalculadora/Especificaciones/Rombo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperacionesCalculadora
+{
+    public class Rombo
+    {
+        public void ResolverArea(double diagonal,double diagonal2,double lado,ref double resultado){
+
+            ref resultado = diagonal * diagonal2 / 2;
+        }
+
+
+    }
+}
