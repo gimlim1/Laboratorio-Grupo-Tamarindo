d15baad58036fd8f8438bd85cfa747caeb1b4c1c
marichalmarco <marichalmarco17@gmail.com>
2015-11-07 16:33:19 -0600

    Cambios 3

---
+++ OperacionesCalculadora/OperacionesCalculadora/Especificaciones/Poligono.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperacionesCalculadora.Especificaciones
+{
+    class Poligono
+    {
+
+        /*  6.	Creen un wcf que reciba:
+  a.un valor que indique si desea calcular el perímetro o el área de un polígono regular de 5 o más lados.La cantidad máxima permitida de lados es 16.
+  b.el número de lados de que consta la figura
+  c.el tamaño de cada uno de ellos (como la figura es regular, todos los lados miden exactamente lo mismo, por lo que sólo se recibe un valor)
+  Además, que devuelva el resultado de calcular ese perímetro o esa área. */
+
+        public double ResolverArea(double lado)
+        {
+            return lado1 * lado1;
+        }
+
+        public double ResolverPerimetro(double lado1)
+        {
+
+            return lado1 * 4;
+        }
+
+
+
+
+    }
+}
