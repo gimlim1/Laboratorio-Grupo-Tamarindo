65c3e4cd19c5a9c99863dfa7748c570078592fe8
gimlim 1 <ricardojosepineda@hotmail.com>
2015-11-07 17:03:50 -0600

    Service1

--- OperacionesCalculadora/WCFTriangulo/ResolverTriangulo.svc.cs
+++ OperacionesCalculadora/WCFTriangulo/ResolverTriangulo.svc.cs
@@ -10,13 +10,13 @@ namespace WCFTriangulo
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    public class ResolverTriangulo : IResolverTriangulo
     {
 
         public double Resolver(double lado1, double lado2, double lado3, string Operacion)
         {
-
-
+            OperacionesCalculadora.Acciones.ResolverTriangulo resolver = new OperacionesCalculadora.Acciones.ResolverTriangulo();
+            return resolver.Resolver(lado1, lado2, lado3, Operacion);
         }
   }
 }
