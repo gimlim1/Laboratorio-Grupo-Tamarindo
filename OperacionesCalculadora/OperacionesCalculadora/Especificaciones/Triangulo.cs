980814ed3d9cb7e71a53de5a52a97db1e0724bc0
marichalmarco <marichalmarco17@gmail.com>
2015-11-07 16:03:05 -0600

    Triangulo Terminado

--- OperacionesCalculadora/OperacionesCalculadora/Especificaciones/Triangulo.cs
+++ OperacionesCalculadora/OperacionesCalculadora/Especificaciones/Triangulo.cs
@@ -6,21 +6,43 @@ using System.Threading.Tasks;
 
 namespace OperacionesCalculadora.Especificaciones
 {
-    class Triangulo
+    public class Triangulo
     {
 
         public double ResolverArea(double lado1, double lado2, double lado3)
         {
-            double semiperimetro = ResolverPerimetro(lado1, lado2, lado3);
-            return Math.Sqrt(semiperimetro * (semiperimetro-lado1) * (semiperimetro-lado2) * (semiperimetro-lado3));
 
+            if (ValidarTriangulo( lado1,  lado2,  lado3) == true)
+            {
+
+                double semiperimetro = ResolverPerimetro(lado1, lado2, lado3);
+                return Math.Sqrt(semiperimetro * (semiperimetro - lado1) * (semiperimetro - lado2) * (semiperimetro - lado3));
+            }
+            return 0;
         }
 
         public double ResolverPerimetro(double lado1, double lado2, double lado3)
         {
-            return lado1 + lado2 + lado3;
+
+            if (ValidarTriangulo(lado1, lado2, lado3) == true)
+            {
+                return lado1 + lado2 + lado3;
+            }
+            return 0;
         }
 
+        public bool ValidarTriangulo(double lado1, double lado2, double lado3) {
+
+            bool validacion = false;
+
+            if (((lado1 + lado2) > lado3) && ((lado1 + lado3) > lado2) && ((lado2 + lado3) > lado1))
+            {
+                validacion = true;
+            }
+
+            return validacion;
+
+        }
 
 
     }
