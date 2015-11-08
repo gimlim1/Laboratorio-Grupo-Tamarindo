f5f3cb8a80d966e9cc48f7c1f8f92ab90255c27f
gimlim 1 <ricardojosepineda@hotmail.com>
2015-11-07 18:06:50 -0600

    prueba

---
+++ OperacionesCalculadora/Pruebas Automatizadas/PruebaTriangulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pruebas_Automatizadas
+{
+    [TestClass]
+    public class PruebaTriangulo
+    {
+        [TestMethod]
+        public void TrianguloInvalido()
+        {
+            double primerlado = -1;
+            double segundolado = 2;
+            double tercerlado = 3;
+            double primeraRaizReal = 0;
+            double segundaRaizReal = 0;
+            double primeraRaizEsperada = 3;
+            double segundaRaizEsperada = -1;
+
+            ResolverEcuacionCuadratica ecuacionCuadratica = new ResolverEcuacionCuadratica();
+            // invoque al método correspondiente
+            ecuacionCuadratica.ResolverLaEcuacionCuadratica(primerNumero,
+                        segundoNumero, tercerNumero, ref primeraRaizReal, ref segundaRaizReal);
+
+            Assert.IsTrue((primeraRaizEsperada == primeraRaizReal) ||
+                          (primeraRaizEsperada == segundaRaizReal));
+            Assert.IsTrue((segundaRaizEsperada == primeraRaizReal) ||
+                          (segundaRaizEsperada == segundaRaizReal));
+        }
+    }
+}
