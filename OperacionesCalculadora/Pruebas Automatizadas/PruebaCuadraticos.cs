ca1c5dec7348692fa20020fec4578516d613be71
gimlim 1 <ricardojosepineda@hotmail.com>
2015-11-07 18:36:32 -0600

    Pruebas

---
+++ OperacionesCalculadora/Pruebas Automatizadas/PruebaCuadraticos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Pruebas_Automatizadas
+{
+    [TestClass]
+    public class PruebaCuadraticos
+    {
+        [TestMethod]
+        public void PruebaMayorquecero()
+        {
+
+
+        }
+ }
+}
---
+++ OperacionesCalculadora/Pruebas Automatizadas/PruebaPoligono.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pruebas_Automatizadas
+{
+    [TestClass]
+    public class PruebaPoligono
+    {
+        [TestMethod]
+        public void PruebaMayorquecero()
+        {
+
+
+        }
+    }
+}
